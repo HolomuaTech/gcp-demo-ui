a1959a30e8fa1f8a2e5bf55305a1a69398b6d9d9
Paul Wheeler <paul@free-side.net>
2024-10-31 00:16:36 -1000

    Adding some fireworks to the demo

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -1,9 +1,10 @@
 import * as React from "react"
 import { render, RenderOptions } from "@testing-library/react"
 import { ChakraProvider, theme } from "@chakra-ui/react"
+import { ApiStatusProvider } from './apiStatusContext';
 
 const AllProviders = ({ children }: { children?: React.ReactNode }) => (
-  <ChakraProvider theme={theme}>{children}</ChakraProvider>
+  <ChakraProvider theme={theme}><ApiStatusProvider>{children}</ApiStatusProvider></ChakraProvider>
 )
 
 const customRender = (ui: React.ReactElement, options?: RenderOptions) =>
